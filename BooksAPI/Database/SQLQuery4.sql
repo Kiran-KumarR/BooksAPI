d3927ab665aa95fd4ddc1eedd83e0539d8e08b54
KIRAN KUMAR R <141916203+Kiran-KumarR@users.noreply.github.com>
2023-09-09 08:16:40 +0530

    Database folder added,along with queries that was used

---
+++ BooksAPI/Database/SQLQuery4.sql
@@ -0,0 +1,33 @@
+CREATE TABLE Author (
+auth_id  int IDENTITY(3000,1) PRIMARY KEY,
+author_name VARCHAR(50)
+);
+
+CREATE TABLE Publisher (
+pub_id  int IDENTITY(2000,1) PRIMARY KEY,
+publisher_name VARCHAR(50)
+);
+
+
+CREATE TABLE Books (
+    id INT PRIMARY KEY,
+    title VARCHAR(50),
+    author_id INT,
+    publisher_id INT,
+    description VARCHAR(1000),
+    FOREIGN KEY (author_id) REFERENCES Author(auth_id),
+    FOREIGN KEY (publisher_id) REFERENCES Publisher(pub_id)
+);
+
+
+CREATE TABLE BookInfo (
+    id INT REFERENCES Books(id) ,
+    language VARCHAR(20),
+    maturityRating VARCHAR(30),
+    pageCount INT,
+    categories VARCHAR(50),
+    publishedDate VARCHAR(50),
+    retailPrice DECIMAL(6, 3)
+);
+
+
---
+++ BooksAPI/Database/SQLQuery7.sql
@@ -0,0 +1,5 @@
+SELECT * FROM Author WHERE auth_id = 3000;
+
+SELECT * FROM Author;
+
+Delete from Author where auth_id=3007;
